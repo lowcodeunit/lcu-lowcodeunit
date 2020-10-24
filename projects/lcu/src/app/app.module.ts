98dc7a1ab081a9b50b300250ec5294f16e5f0897
mcgear <michael.gearhardt@gmail.com>
2020-10-23 18:52:31 -0600

    Initial check in

---
+++ projects/lcu/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FathymSharedModule, LCUServiceSettings } from '@lcu/common';
+import { environment } from '../environments/environment';
+import { LcuSetupModule } from '@iot-ensemble/lcu-setup-common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FathymSharedModule,
+    LcuSetupModule.forRoot()
+  ],
+  providers: [
+    {
+      provide: LCUServiceSettings,
+      useValue: FathymSharedModule.DefaultServiceSettings(environment)
+    }
+  ],
+  exports: [LcuSetupModule]
+})
+export class AppModule {}
