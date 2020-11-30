221776e76dc5b728af3c2ab06631d38682202b1a
mcgear <michael.gearhardt@gmail.com>
2020-11-30 12:49:19 -0700

    fix demo a;;

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FathymSharedModule, MaterialModule } from '@lcu/common';
+import { FathymSharedModule, LCUServiceSettings, MaterialModule } from '@lcu/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './controls/home/home.component';
@@ -12,23 +12,24 @@ import { LcuSetupModule } from '@iot-ensemble/lcu-setup-common';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    DocumentationComponent
-  ],
+  declarations: [AppComponent, HomeComponent, DocumentationComponent],
   imports: [
-  AppRoutingModule,
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
-    FathymSharedModule,
+    FathymSharedModule.forRoot(),
     MaterialModule,
     FlexLayoutModule,
     LcuDocumentationModule.forRoot(),
-    LcuSetupModule.forRoot(environment)
+    LcuSetupModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LCUServiceSettings,
+      useValue: FathymSharedModule.DefaultServiceSettings(environment),
+    },
+  ],
   bootstrap: [AppComponent],
-  exports: [LcuSetupModule]
+  exports: [LcuSetupModule],
 })
-export class AppModule { }
+export class AppModule {}
