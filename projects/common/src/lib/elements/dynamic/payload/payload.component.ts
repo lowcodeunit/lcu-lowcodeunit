468377d3ba053f458b3f6b35ebc1e83e335e8e12
xchanin <shannonwbruns@gmail.com>
2020-12-05 13:22:33 -0700

    AB#8917 fix missing style import

--- projects/common/src/lib/elements/dynamic/payload/payload.component.ts
+++ projects/common/src/lib/elements/dynamic/payload/payload.component.ts
@@ -8,18 +8,19 @@ import { IoTEnsembleTelemetryPayload } from '../../../state/iot-ensemble.state';
 })
 export class PayloadComponent implements OnInit {
 
+  /**
+   * DataSource is required in order to show row data within this component
+   */
   private _datasource: IoTEnsembleTelemetryPayload;
   // tslint:disable-next-line:no-input-rename
   @Input('datasource')
   set DataSource(val: IoTEnsembleTelemetryPayload) {
-    debugger;
     this._datasource = val;
   }
 
   get DataSource(): IoTEnsembleTelemetryPayload {
     return this._datasource;
   }
-
 
   constructor() { }
 
