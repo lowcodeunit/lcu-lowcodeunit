c0221648344e00c9ba33930edc99690678e87f6a
xchanin <shannonwbruns@gmail.com>
2020-12-18 16:57:04 -0700

    AB#8779 more changes to generic modal

--- projects/common/src/lcu.api.ts
+++ projects/common/src/lcu.api.ts
@@ -8,7 +8,6 @@ export * from './lib/state/iot-ensemble-state.context';
 export * from './lib/utils/breakpoint-utils.service';
 export * from './lib/animations/animations';
 export * from './lib/services/sidenav.service';
-export * from './lib/services/generic-modal.service';
 export * from './lib/models/generice-modal.model';
 export * from './lib/elements/generic/generic-modal/generic-modal.component';
 export * from './lib/elements/controls/payload-form/payload-form.component';
